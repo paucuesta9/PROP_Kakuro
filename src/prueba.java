ac2d37fc5d85df7ac0119e0e4c37d0dae96f90cd
pau.cuesta.arcos <pau.cuesta.arcos@est.fib.upc.edu>
2020-10-09 12:50:33 +0200

    Primer ejemplo

---
+++ src/prueba.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class prueba {
+    private String nombre;
+    private int num;
+    private List<Integer> lista_nums;
+
+    public prueba(String nombre, int num, List<Integer> lista_nums) {
+        this.nombre = nombre;
+        this.num = num;
+        this.lista_nums = lista_nums;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public List<Integer> getLista_nums() {
+        return lista_nums;
+    }
+
+    public void setLista_nums(List<Integer> lista_nums) {
+        this.lista_nums = lista_nums;
+    }
+}
