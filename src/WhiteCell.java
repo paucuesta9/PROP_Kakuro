9f6afba9175eb9e9968f32f8e2494c0503fffd28
pau.cuesta.arcos <pau.cuesta.arcos@est.fib.upc.edu>
2020-10-12 16:20:46 +0200

    WhiteCell class creada
    Se ha creado la subclase WhiteCell con sus tres atributos, junto con la creadora por defecto y los getters and setters de cada atributo. Faltará extenderla de la Clase Cell cuando esté creada y llamar al padre en la constructora

---
+++ src/WhiteCell.java
@@ -0,0 +1,46 @@
+/* Pau Cuesta Arcos */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WhiteCell {
+    private int value;
+    private int correctValue;
+    private List<Boolean> tempValues;
+
+    public WhiteCell() {
+        tempValues = new ArrayList<>(9);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getCorrectValue() {
+        return correctValue;
+    }
+
+    public void setCorrectValue(int correctValue) {
+        this.correctValue = correctValue;
+    }
+
+    public List<Boolean> getTempValues() {
+        return this.tempValues;
+    }
+
+    public Boolean getTempValue(int index) {
+        return this.tempValues.get(index);
+    }
+
+    public void setTempValues(List<Boolean> tempValues) {
+        this.tempValues = tempValues;
+    }
+
+    public void setTempValue(int index, Boolean value) {
+        this.tempValues.set(index, value);
+    }
+}
