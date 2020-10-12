3ae19a7b34f657c121de4190a416dfb3dff70567
judith.almono <judith.almono@est.fib.upc.edu>
2020-10-12 17:50:10 +0200

    Creación clase BlackCell con los getters y setters

---
+++ src/BlackCell.java
@@ -0,0 +1,26 @@
+public class BlackCell{
+    private int vertical;
+    private int horizontal;
+
+    public void BlackCell(int vertical, int horizontal) {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+    }
+
+    public int getVertical() {
+        return vertical;
+    }
+
+    public int getHorizontal() {
+        return horizontal;
+    }
+
+    public void setVertical(int vertical) {
+        this.vertical = vertical;
+    }
+
+    public void setHorizontal(int horizontal) {
+        this.horizontal = horizontal;
+    }
+
+}
