e31f76ba04ce2558074c36140c3515764fa644c3
pol.vallespi.soro <pol.vallespi.soro@estudiantat.upc.edu>
2020-10-13 18:05:10 +0200

    Clase Cell creada

---
+++ src/Cell.java
@@ -0,0 +1,17 @@
+public class Cell {
+    private int posX;
+    private int posY;
+
+    public Cell(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void setPosX(int posX) { this.posX = posX; }
+
+    public int getPosX() { return posX; }
+
+    public void setPosY(int posY) { this.posY = posY; }
+
+    public int getPosY() { return posY; }
+}
