0b684a2d2ac90a1fd4721b73725b2cac7a76efeb
pau.cuesta.arcos <pau.cuesta.arcos@est.fib.upc.edu>
2020-10-16 12:33:26 +0200

    Creación de clase Main y del CtrlDomain

---
+++ src/CtrlDomain.java
@@ -0,0 +1,9 @@
+public class CtrlDomain {
+
+    public CtrlDomain() {
+    }
+
+    public void play() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public public static void main(String[] args) {
+        CtrlDomain cd = new CtrlDomain();
+        cd.play();
+    }
+}
