49e7347af2b27e68db032abaea24b3d3be75d68f
pau.cuesta.arcos <pau.cuesta.arcos@est.fib.upc.edu>
2020-11-15 20:21:22 +0100

    Driver BlackCell sin leer de fichero

--- src/domain/controllers/drivers/DriverBlackCell.java
+++ src/domain/controllers/drivers/DriverBlackCell.java
@@ -13,56 +13,88 @@ public class DriverBlackCell {
     }
 
     private static void testCreadoraConValor(int row, int column) {
-        BlackCell bc = new BlackCell(row, column);
+        BlackCell bc = new BlackCell(column, row);
         System.out.println("Row = " + bc.getRow() + "\n Column = " + bc.getColumn());
     }
 
-    private static void testGetRow() {
-
+    private static void testGetRow(int row) {
+        BlackCell bc = new BlackCell(0, row);
+        System.out.println("Row = " + bc.getRow());
     }
 
-    private static void testGetColumn() {
-
+    private static void testGetColumn(int column) {
+        BlackCell bc = new BlackCell(column, 0);
+        System.out.println("Column = " + bc.getColumn());
     }
 
-    private static void testSetRow() {
-
+    private static void testSetRow(int row) {
+        BlackCell bc = new BlackCell();
+        bc.setRow(row);
+        System.out.println("Row = " + bc.getRow());
     }
 
-    private static void testSetColumn() {
-
+    private static void testSetColumn(int column) {
+        BlackCell bc = new BlackCell();
+        bc.setRow(column);
+        System.out.println("Column = " + bc.getColumn());
     }
 
     public static void main(String[] args) {
         System.out.println("Opciones: \n 1. Creadora sin valor \n 2. Creadora con valor \n 3. Getter de row \n 4. Getter de column \n 5. Setter de row \n 6. Setter de column \n 7. Salir");
         int value = readNumber();
         while (value != 7) {
+            int x, y;
             switch (value) {
                 case 1:
                     testCreadoraSinValor();
                     break;
                 case 2:
                     System.out.println("Indique la suma de fila y la suma de columna o escriba -1 para usar fichero");
-                    int x = reader.nextInt();
-                    int y;
+                    x = reader.nextInt();
                     if (x == -1) {
-
+                        x = 0;
+                        y = 0;
+                        //TODO: Leer de fichero
                     } else {
                         y = reader.nextInt();
                     }
-                    //testCreadoraConValor(x, y);
+                    testCreadoraConValor(x, y);
                     break;
                 case 3:
-                    testGetRow();
+                    System.out.println("Indique la suma de fila o escriba -1 para usar fichero");
+                    x = reader.nextInt();
+                    if (x == -1) {
+                        x = 0;
+                        //TODO: Leer de fichero
+                    }
+                    testGetRow(x);
                     break;
                 case 4:
-                    testGetColumn();
+                    System.out.println("Indique la suma de columna o escriba -1 para usar fichero");
+                    x = reader.nextInt();
+                    if (x == -1) {
+                        x = 0;
+                        //TODO: Leer de fichero
+                    }
+                    testGetColumn(x);
                     break;
                 case 5:
-                    testSetRow();
+                    System.out.println("Indique la suma de fila o escriba -1 para usar fichero");
+                    x = reader.nextInt();
+                    if (x == -1) {
+                        x = 0;
+                        //TODO: Leer de fichero
+                    }
+                    testSetRow(x);
                     break;
                 case 6:
-                    testSetColumn();
+                    System.out.println("Indique la suma de columna o escriba -1 para usar fichero");
+                    x = reader.nextInt();
+                    if (x == -1) {
+                        x = 0;
+                        //TODO: Leer de fichero
+                    }
+                    testSetColumn(x);
                     break;
             }
             System.out.println("\nOpciones: \n 1. Creadora sin valor \n 2. Creadora con valores \n 3. Obtener tamaño fila \n 4. Obtener tamaño columna \n 5. Introducir tamaño fila \n 6. Introducir tamaño columna \n 7. Salir");
