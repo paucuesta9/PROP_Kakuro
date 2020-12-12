cc31a859f3f0fc51faa5cd3244690d5cb2a4e787
alvaro.armada <alvaro.armada@est.fib.upc.edu>
2020-12-12 14:23:34 +0100

    Pairs.

---
+++ src/domain/classes/Pair.java
@@ -0,0 +1,43 @@
+package domain.classes;
+
+import java.util.Objects;
+
+public class Pair {
+    private int first;
+    private int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return first == pair.first &&
+                second == pair.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
