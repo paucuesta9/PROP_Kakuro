61e6ff25c66e7ecf2c677a04b3d3e61d3a41cb85
alvaro.armada <alvaro.armada@est.fib.upc.edu>
2020-12-19 17:32:13 +0100

    Doxygen

--- src/domain/classes/Config.java
+++ src/domain/classes/Config.java
@@ -53,17 +53,17 @@ public class Config {
         volume = -10;
     }
 
-    /**
+    /** @brief Creadora de Config
      * 
-     * @param colorIncorrectCell
-     * @param volume
-     * @param colorNumbersBlackCell
-     * @param colorSelCell
-     * @param colorNumbersWhiteCell
-     * @param colorBlackCell
-     * @param colorBorde
-     * @param colorWhiteCell
-     * @param colorCorrectCell
+     * @param colorIncorrectCell color de las celdas incorrectas
+     * @param volume valor del volumen de la música
+     * @param colorNumbersBlackCell color de los números de las celdas negras
+     * @param colorSelCell color de la celda seleccionada
+     * @param colorNumbersWhiteCell color de los números de las celdas blancas
+     * @param colorBlackCell color de las celdas negras
+     * @param colorBorde color del borde entre casillas y de los número que se escriben la función lapiz
+     * @param colorWhiteCell color de las celdas blancas
+     * @param colorCorrectCell color de las celdas con valor correcto
      */
     public Config(String colorBlackCell, String colorWhiteCell, String colorNumbersBlackCell, String colorNumbersWhiteCell, String colorCorrectCell, String colorIncorrectCell, String colorSelCell, String colorBorde, int volume) {
         super();
@@ -78,78 +78,154 @@ public class Config {
         this.volume = volume;
     }
 
+    /** @brief Getter del color de la celda negra
+     *
+     * @return string que representa el color de las celdas negras
+     */
     public String getColorBlackCell() {
         return colorBlackCell;
     }
 
+    /** @brief Setter del color de la celda negra
+     *
+     * @param colorBlackCell string que representa el color de las celdas negras
+     */
     public void setColorBlackCell(String colorBlackCell) {
         this.colorBlackCell = colorBlackCell;
     }
 
+    /** @brief Getter del color de las celdas blancas
+     *
+     * @return string que representa el color de las celdas blancas
+     */
     public String getColorWhiteCell() {
         return colorWhiteCell;
     }
 
+    /** @brief Setter del color de las celdas blancas
+     *
+     * @param colorWhiteCell string que representa el color de las celdas blancas
+     */
     public void setColorWhiteCell(String colorWhiteCell) {
         this.colorWhiteCell = colorWhiteCell;
     }
 
+    /** @brief Getter del color de los números de las casillas negras
+     *
+     * @return string que representa el color de los números de las casillas negras
+     */
     public String getColorNumbersBlackCell() {
         return colorNumbersBlackCell;
     }
 
+    /** @brief Setter del color de los números de las casillas negras
+     *
+     * @param colorNumbersBlackCell string que representa el color de los números de las casillas negras
+     */
     public void setColorNumbersBlackCell(String colorNumbersBlackCell) {
         this.colorNumbersBlackCell = colorNumbersBlackCell;
     }
 
+    /** @brief Getter del color de los números de las casillas blancas
+     *
+     * @return string que representa el color de los números de las casillas blancas
+     */
     public String getColorNumbersWhiteCell() {
         return colorNumbersWhiteCell;
     }
 
+    /** @brief Setter del color de los números de las casillas negras
+     *
+     * @param colorNumbersWhiteCell string que representa el color de los números de las casillas blancas
+     */
     public void setColorNumbersWhiteCell(String colorNumbersWhiteCell) {
         this.colorNumbersWhiteCell = colorNumbersWhiteCell;
     }
 
+    /** @brief Getter del color de las celdas con valor correcto
+     *
+     * @return string que representa el color de las celdas con valor correcto
+     */
     public String getColorCorrectCell() {
         return colorCorrectCell;
     }
 
+    /** @brief Setter del color de las celdas con valor correcto
+     *
+     * @param colorCorrectCell string que representa el color de las celdas con valor correcto
+     */
     public void setColorCorrectCell(String colorCorrectCell) {
         this.colorCorrectCell = colorCorrectCell;
     }
 
+    /** @brief Getter del color de las celdas con valor incorrecto
+     *
+     * @return string que representa el color de las celdas con valor incorrecto
+     */
     public String getColorIncorrectCell() {
         return colorIncorrectCell;
     }
 
+    /** @brief Setter del color de las celdas con valor incorrecto
+     *
+     * @param colorIncorrectCell string que representa el color de las celdas con valor incorrecto
+     */
     public void setColorIncorrectCell(String colorIncorrectCell) {
         this.colorIncorrectCell = colorIncorrectCell;
     }
 
+    /** @brief Getter del color de la celda seleccionada
+     *
+     * @return string que representa el color de la celda seleccionada
+     */
     public String getColorSelCell() {
         return colorSelCell;
     }
 
+    /** @brief Setter del color de la celda seleccionada
+     *
+     * @param colorSelCell string que representa el color de la celda seleccionada
+     */
     public void setColorSelCell(String colorSelCell) {
         this.colorSelCell = colorSelCell;
     }
 
+    /** @brief Getter del color del borde entre casillas y de los números a lapiz
+     *
+     * @return string que representa el color del borde entre casillas y de los número a lápiz
+     */
     public String getColorBorde() {
         return colorBorde;
     }
 
+    /** @brief Setter del color del borde entre casillas y de los números a lapiz
+     *
+     * @param colorBorde string que representa el color del borde entre casillas y de los número a lápiz
+     */
     public void setColorBorde(String colorBorde) {
         this.colorBorde = colorBorde;
     }
 
+    /** @brief Getter del valor del volumen de la música
+     *
+     * @return string que representa el valor del volumen de la música
+     */
     public int getVolume() {
         return volume;
     }
 
+    /** @brief Setter del valor del volumen de la música
+     *
+     * @param volume string que representa el valor del volumen de la música
+     */
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
+    /** @brief Getter de todos los atributos de la clase
+     *
+     * @return ArrayList de todos los atributos de la clase
+     */
     public ArrayList<String> getConfig() {
         ArrayList config = new ArrayList();
         config.add(colorBlackCell);
@@ -164,6 +240,9 @@ public class Config {
         return config;
     }
 
+    /** @brief Pone todos los atributos de la clase con su valor por defecto
+     *
+     */
     public void resetColors() {
         colorBlackCell = "#000000";
         colorWhiteCell = "#FFFFFF";
--- src/domain/classes/Pair.java
+++ src/domain/classes/Pair.java
@@ -2,31 +2,67 @@ package domain.classes;
 
 import java.util.Objects;
 
+/** @file Pair.java
+ @brief Clase  <em>Pair</em>.
+ */
+
+/** @brief Clase de pair, donde ambos elementos son enteros
+ * @author -----
+ */
+
 public class Pair {
     private int first;
     private int second;
+
+    /** @brief Creadora de Pair
+     *
+     * @param first primer entero
+     * @param second segundo entero
+     */
 
     public Pair(int first, int second) {
         this.first = first;
         this.second = second;
     }
 
+    /** @brief getter del primer entero
+     *
+     * @return el primer entero
+     */
     public int getFirst() {
         return first;
     }
 
+    /** @brief setter del primer entero
+     *
+     * @param first valor para el primer entero
+     */
     public void setFirst(int first) {
         this.first = first;
     }
+
+    /** @brief getter del segundo entero
+     *
+     * @return el segundo entero
+     */
 
     public int getSecond() {
         return second;
     }
 
+    /** @brief setter del segundo entero
+     *
+     * @param second valor para el segundo entero
+     */
     public void setSecond(int second) {
         this.second = second;
     }
 
+    /** @brief función para saber si dos Pairs son iguales
+     *
+     * @param o Pair a comparar
+     * @return cierto si son iguales, falso en caso contrario
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,6 +72,10 @@ public class Pair {
                 second == pair.second;
     }
 
+    /** @brief función para poder introducir Pairs en tablas de Hash y otras estructuras de datos
+     *
+     * @return identifacador de hash del pair.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(first, second);
